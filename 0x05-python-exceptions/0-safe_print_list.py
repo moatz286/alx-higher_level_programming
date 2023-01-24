776e9472b39b7150aead23c6cf290cf91368984a
Mutaz Abd Alrhman <moatz286@gmail.com>
2023-01-23 16:20:29 -0800

    python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-
-my_list = [1, 2, 3, 4, 5]
-
 def safe_print_list(my_list=[], x=0):
-	y =0
-	while True:
-		try:
-			if y < x:
-				print(my_list[y], end='')
-				y += 1
-			else:
-				print()
-				return y
-		except IndexError:
-        		print()
-        		return y
+    index = 0
+    while True:
+        try:
+            if index < x:
+                print(my_list[index], end='')
+                index += 1
+            else:
+                print()
+                return index
+        except IndexError:
+            print()
+            return index
