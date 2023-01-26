66e3bf8bcf268d84acd1bfaa23a864a46ba2725d
Mutaz Abd Alrhman <moatz286@gmail.com>
2023-01-26 02:49:06 -0800

    classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,3 +3,4 @@
 
 class Square:
     """An empty class."""
+    pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
+""" Square class."""
+
 class Square:
-    """
-    Square class with private instance attribute size
-    """
+    """Square class with private instance attribute size"""
+
     def __init__(self, size):
+        """init function 
+
+        Args:
+            size (int): int represent the square size.
+
+        """
         self._size = size
