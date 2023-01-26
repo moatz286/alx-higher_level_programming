63923b26831fbbf64ede809878a174dabd3631ab
Mutaz Abd Alrhman <moatz286@gmail.com>
2023-01-26 05:10:58 -0800

    fix

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+Square = __import__('3-square').Square
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
 
 try:
-    print(my_square.size)
+    print(my_square_1.size)
 except Exception as e:
     print(e)
 
 try:
-    print(my_square.__size)
+    print(my_square_1.__size)
 except Exception as e:
     print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
